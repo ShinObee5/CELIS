36835cbaadfffe5fa19887b927e4acb76f77e225
NarayanAdithya <narayanadithya1234@gmail.com>
2020-08-20 22:02:54 +0530

    Flask Checks

--- celis_app/app/routes.py
+++ celis_app/app/routes.py
@@ -7,3 +7,7 @@ from flask import request,redirect,url_for,render_template,flash
 @app.route('/index')
 def index():
     return render_template('index.html',title='Home')
+
+@app.route('/basetemplate')
+def base():
+    return render_template('template.html',title='template')
