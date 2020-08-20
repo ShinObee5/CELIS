155a85cc09cb017e1e5eb4d6b65ec967c189cc68
NarayanAdithya <narayanadithya1234@gmail.com>
2020-08-20 21:55:56 +0530

    Flask Structure Init

---
+++ celis_app/app/routes.py
@@ -0,0 +1,9 @@
+from app import app
+from flask import request,redirect,url_for,render_template,flash
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html',title='Home')
