f79d2e64873c84b93a95180f1e182b375c1fb55d
NarayanAdithya <narayanadithya1234@gmail.com>
2020-08-21 22:33:19 +0530

    Removed unnecessary files

--- celis_app/app/routes.py
+++ celis_app/app/routes.py
@@ -3,10 +3,10 @@ from flask import request,redirect,url_for,render_template,flash
 
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html',title='Home')
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return render_template('index.html',title='Home')
 
 @app.route('/basetemplate')
 def base():
