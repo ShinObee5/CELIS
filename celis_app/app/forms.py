829cbb41ef913381120ac6d52a7950c64477b4f0
NarayanAdithya <narayanadithya1234@gmail.com>
2020-09-26 12:02:02 +0530

    Major Template Changes

--- celis_app/app/forms.py
+++ celis_app/app/forms.py
@@ -4,7 +4,7 @@ from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired,Email,EqualTo
 
 
-class LoginForm(FlaskForm):
+class RegisterForm(FlaskForm):
     username=StringField('Username',validators=[DataRequired()],render_kw={'class':'form-control form-group'})
     email=EmailField('Email',validators=[DataRequired(),Email()],render_kw={'class':'form-control form-group'})
     password=PasswordField('Password',validators=[DataRequired()],render_kw={'class':'form-control form-group'})
@@ -12,4 +12,10 @@ class LoginForm(FlaskForm):
     user_role=RadioField('User Role',validators=[DataRequired()],choices=[('Student','Student'),('Instructor','Instructor')],render_kw={'class':'form-check form-check-input','style':'list-style:none;'})
     Region=SelectField('Select Region',validators=[(DataRequired())],choices=[('USA','USA'),('India','India'),('America','America')],render_kw={'class':'form-group col-md-4 form-control '})
     # remember_me=BooleanField('Keep Me Signed In')
+    submit=SubmitField('Sign In',render_kw={'class':'btn btn-primary','style':'height : 50px;'})
+
+class LoginForm(FlaskForm):
+    email=EmailField('Email',validators=[DataRequired(),Email()],render_kw={'class':'form-control form-group'})
+    password=PasswordField('Password',validators=[DataRequired()],render_kw={'class':'form-control form-group'})
+    remember_me=BooleanField('Keep Me Signed In')
     submit=SubmitField('Sign In',render_kw={'class':'btn btn-primary','style':'height : 50px;'})
--- celis_app/app/routes.py
+++ celis_app/app/routes.py
@@ -1,13 +1,26 @@
 from app import app
 from flask import request,redirect,url_for,render_template,flash,get_flashed_messages,flash
 from app import forms
-from flask_login import current_user,login_user
+from flask_login import current_user,login_user,logout_user
 from app.models import User
+from app.forms import LoginForm,RegisterForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('celis.html',title='Home')
+
+@app.route('/courses')
+def course():
+    return render_template('courses.html',title='Courses')
+
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -21,18 +34,18 @@ def login():
             return redirect(url_for('login'))
         login_user(user,remember_me=form.remember_me.data)
         return redirect(url_for('index'))
-    return render_template('login.html',title='SignIn',form=form)
+    return render_template('signinpage.html',title='SignIn',form=form)
 
 
 
 @app.route('/basetemplate')
 def base():
-    return render_template('celis.html',title='template')
+    return render_template('template.html',title='template')
 
 
 @app.route('/register',methods=['POST','GET'])
-def sigin():
-    form=forms.LoginForm()
+def register():
+    form=forms.RegisterForm()
     if form.validate_on_submit():
         flash('Login requested for user {}'.format(form.username.data))
         print(form.username.data)
@@ -40,4 +53,9 @@ def sigin():
         print(form.user_role.data)
         print(form.Region.data)
         return redirect('/')
-    return render_template('signuppage.html',form=form)
+    return render_template('signuppage.html',form=form,title='Register')
+
+
+@app.route('/contact')
+def contactus():
+    return render_template('contactus.html',title='Contact Us')
